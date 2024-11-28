be41d6e3c863c7ce0c0af0d81593084334076617
Ritwiz <madangopalsingh2803@gmail.com>
2024-11-28 20:00:45 +0530

    Routing completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,34 @@
 import 'package:flutter/material.dart';
+import 'screens/splashscreen.dart';
+import 'screens/homescreen.dart';
+import 'screens/searchscreen.dart';
+import 'screens/detailsscreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MovieApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MovieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie Explorer',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          elevation: 0,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        '/home': (context) => HomeScreen(),
+        '/search': (context) => SearchScreen(),
+        '/details': (context) => DetailsScreen(),
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Placeholder();
   }
 }
